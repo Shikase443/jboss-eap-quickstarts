eefa46acbd10db5069c78d48ba23bb79790b8776
Y.Shikase <76500448+Shikase443@users.noreply.github.com>
2021-05-13 17:15:55 +0900

    Update HelloService.java

--- helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
+++ helloworld/src/main/java/org/jboss/as/quickstarts/helloworld/HelloService.java
@@ -25,7 +25,7 @@ package org.jboss.as.quickstarts.helloworld;
 public class HelloService {
 
     String createHelloMessage(String name) {
-        return "おはよう " + name + "!";
+        return "Good Morning " + name + "!";
     }
 
 }
